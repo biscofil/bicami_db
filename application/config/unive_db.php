9f49f472afb50bcb4a5ddfa2eb07d6272bfb93b1
Filippo Bisconcin <filippo.bisconcin@gmail.com>
2016-12-20 11:32:22 +0100

    db

---
+++ application/config/unive_db.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['unive_db'] = array(
+    'server' => '*****.*****.unive.it',
+    'port' => '****',
+    'username' => '**********',
+    'password' => '**********',
+    'database' => '**********'
+);
